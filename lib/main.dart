c86aa593726aa136dd8b06721c0cb532608a29dd
milad <78633834+Milad-heydarloo@users.noreply.github.com>
2024-08-17 01:27:39 +0330

    qr_code_scanner
    QR Codes
    Data Matrix
    Aztec
    PDF417
    Barcodes مثل UPC, EAN, Code 39, Code 93, Code 128, Codabar, ITF, RSS-14, RSS Expanded

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,226 @@ import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: const QRViewExample(),
+//     );
+//   }
+// }
+//
+// class QRViewExample extends StatefulWidget {
+//   const QRViewExample({Key? key}) : super(key: key);
+//
+//   @override
+//   State<StatefulWidget> createState() => _QRViewExampleState();
+// }
+//
+// class _QRViewExampleState extends State<QRViewExample> {
+//   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+//   String scannedCode = 'Scan a code';
+//   QRViewController? controller;
+//
+//   @override
+//   void reassemble() {
+//     super.reassemble();
+//     controller?.pauseCamera();
+//     controller?.resumeCamera();
+//   }
+//
+//   Future<void> _requestCameraPermission() async {
+//     var status = await Permission.camera.status;
+//     if (!status.isGranted) {
+//       if (await Permission.camera.request().isGranted) {
+//         // Permission granted
+//       } else {
+//         // Permission denied
+//         ScaffoldMessenger.of(context).showSnackBar(
+//           const SnackBar(content: Text('Camera permission is required to scan QR codes')),
+//         );
+//       }
+//     }
+//   }
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _requestCameraPermission();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('QR Code Scanner'),
+//       ),
+//       body: Column(
+//         children: <Widget>[
+//           Expanded(
+//             flex: 5,
+//             child: QRView(
+//               key: qrKey,
+//               onQRViewCreated: _onQRViewCreated,
+//             ),
+//           ),
+//           Expanded(
+//             flex: 1,
+//             child: Center(
+//               child: Text(scannedCode),
+//             ),
+//           ),
+//           ElevatedButton(
+//             onPressed: () {
+//               _requestCameraPermission().then((_) {
+//                 controller?.resumeCamera();
+//               });
+//             },
+//             child: const Text('Open Camera'),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+//
+//   void _onQRViewCreated(QRViewController controller) {
+//     this.controller = controller;
+//     controller.scannedDataStream.listen((scanData) {
+//       setState(() {
+//         scannedCode = scanData.code ?? 'Unknown';
+//         controller.pauseCamera();
+//       });
+//     });
+//   }
+//
+//   @override
+//   void dispose() {
+//     controller?.dispose();
+//     super.dispose();
+//   }
+// }
+
+
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: const QRViewExample(),
+//     );
+//   }
+// }
+//
+// class QRViewExample extends StatefulWidget {
+//   const QRViewExample({Key? key}) : super(key: key);
+//
+//   @override
+//   State<StatefulWidget> createState() => _QRViewExampleState();
+// }
+//
+// class _QRViewExampleState extends State<QRViewExample> {
+//   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+//   String scannedCode = 'Scan a code';
+//   QRViewController? controller;
+//
+//   @override
+//   void reassemble() {
+//     super.reassemble();
+//     controller?.pauseCamera();
+//     controller?.resumeCamera();
+//   }
+//
+//   Future<void> _requestCameraPermission() async {
+//     var status = await Permission.camera.status;
+//     if (!status.isGranted) {
+//       if (await Permission.camera.request().isGranted) {
+//         // Permission granted
+//       } else {
+//         // Permission denied
+//         ScaffoldMessenger.of(context).showSnackBar(
+//           const SnackBar(content: Text('Camera permission is required to scan QR codes')),
+//         );
+//       }
+//     }
+//   }
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _requestCameraPermission();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('QR Code Scanner'),
+//       ),
+//       body: Column(
+//         children: <Widget>[
+//           Expanded(
+//             flex: 5,
+//             child: QRView(
+//               key: qrKey,
+//               onQRViewCreated: _onQRViewCreated,
+//             ),
+//           ),
+//           Expanded(
+//             flex: 1,
+//             child: Center(
+//               child: Text(scannedCode),
+//             ),
+//           ),
+//           ElevatedButton(
+//             onPressed: () {
+//               _requestCameraPermission().then((_) {
+//                 controller?.resumeCamera();
+//               });
+//             },
+//             child: const Text('Open Camera'),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+//
+//   void _onQRViewCreated(QRViewController controller) {
+//     this.controller = controller;
+//     controller.scannedDataStream.listen((scanData) {
+//       setState(() {
+//         scannedCode = scanData.code ?? 'Unknown';
+//         controller.pauseCamera();
+//       });
+//     });
+//   }
+//
+//   @override
+//   void dispose() {
+//     controller?.dispose();
+//     super.dispose();
+//   }
+// }
+
+
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -28,6 +248,7 @@ class _QRViewExampleState extends State<QRViewExample> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   String scannedCode = 'Scan a code';
   QRViewController? controller;
+  bool isFlashOn = false; // متغیر برای ذخیره وضعیت فلش
 
   @override
   void reassemble() {
@@ -73,17 +294,36 @@ class _QRViewExampleState extends State<QRViewExample> {
           ),
           Expanded(
             flex: 1,
-            child: Center(
-              child: Text(scannedCode),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(scannedCode),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        _requestCameraPermission().then((_) {
+                          controller?.resumeCamera();
+                        });
+                      },
+                      child: const Text('Open Camera'),
+                    ),
+                    const SizedBox(width: 16),
+                    ElevatedButton(
+                      onPressed: () async {
+                        await controller?.toggleFlash();
+                        setState(() {
+                          isFlashOn = !isFlashOn; // وضعیت فلش را تغییر می‌دهد
+                        });
+                      },
+                      child: Text(isFlashOn ? 'Turn Flash Off' : 'Turn Flash On'),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              _requestCameraPermission().then((_) {
-                controller?.resumeCamera();
-              });
-            },
-            child: const Text('Open Camera'),
           ),
         ],
       ),
